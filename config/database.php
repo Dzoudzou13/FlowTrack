62201e921c6aef6d01fb0e7960415dd0a897c5e3
Jozef Kušnierik <jozfkusnierik2@gmail.com>
2026-04-02 06:56:56 +0200

    correct MySQL connection settings
    Update database configuration to use the correct MySQL port for the FlowTrack app.

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'host' => '127.0.0.1',
+    'port' => 8889,
+    'dbname' => 'flowtrack',
+    'charset' => 'utf8mb4',
+    'username' => 'root',
+    'password' => 'root',
+];
+
